9c464bfdd12cb677ab0849dfebe13a9ebad7a4b6
ingrid1802 <ingridaybar841@gmail.com>
2023-07-31 11:00:34 -0500

    creacion de login

--- src/component/login.js
+++ src/component/login.js
@@ -1,14 +1,67 @@
+import { createElement } from '../utils/utils';
+
 export function loginView() {
-  const content = document.createElement('section');
-  content.classList.add('content-principal');
-  const title = document.createElement('h1');
-  title.id = 'titulo';
-  title.innerHTML = 'Bienvenido Login';
-  content.appendChild(title);
+  const content = document.createElement('main');
+  content.classList.add('mimain');
+
+  // Crear el elemento <section> con la clase "section-img"
+  const sectionImgElement = createElement('section', 'section-img', content);
+
+  // Crear el elemento <img> y configurar sus atributos src y alt
+  const imgElement = createElement('img', 'img-logo1', sectionImgElement);
+  imgElement.src = 'img/logo-principal.png';
+  imgElement.alt = 'logo FandomFlix';
+
+  // Crear el elemento <form> con el identificador "formRegistro1"
+  const formElement = createElement('form', '', content);
+  formElement.id = 'formRegistro1';
+
+  // crear un titulo a mi form
+  const title = createElement('p', 'title1', formElement);
+  title.innerHTML = 'Signin';
+
+  // input text nombre
+  const nameDiv = createElement('div', 'input-iconos', formElement);
+  const nameIcono = createElement('p', 'estilos-icono', nameDiv);
+  nameIcono.innerHTML = '<i class="fa-solid fa-user"></i>';
+  const nameText = createElement('input', 'estilos-input', nameDiv);
+  nameText.setAttribute('type', 'text');
+  nameText.placeholder = 'Nombre de Usuario';
+
+  // input text password
+  const passDiv = createElement('div', 'input-iconos', formElement);
+  const passIcono = createElement('p', 'estilos-icono', passDiv);
+  passIcono.innerHTML = '<i class="fa-solid fa-lock"></i>';
+  const passText = createElement('input', 'estilos-input', passDiv);
+  passText.setAttribute('type', 'password');
+  passText.placeholder = 'Contraseña';
+
+  // boton de iniciar sesion
+  const btnIniciarSesion = createElement('button', 'btnIniciarSesion', formElement);
+  btnIniciarSesion.setAttribute('type', 'submit');
+  btnIniciarSesion.innerHTML = 'Iniciar Seción <i class="fa-solid fa-right-to-bracket"></i>';
+
+  // navegacion a la pagina de registro
+  const mensajeNoTienesCuenta = createElement('p', 'mensajeNoTienesCuenta', formElement);
+  mensajeNoTienesCuenta.textContent = 'Si no tienes cuenta';
+
+  const registrate = createElement('span', 'Registrate', mensajeNoTienesCuenta);
+  registrate.textContent = 'Registrate';
+
+  // seccion del boton de registro cuenta de Google
+  const btnGoogle = createElement('button', 'btnGoogle', formElement);
+  btnGoogle.innerHTML = '<img src="/img/google.png" alt="cuenta gmail">Google';
 
   /* -------------------------------Navegacion vista registro--------------------------------- */
-  title.addEventListener('click', () => {
+  registrate.addEventListener('click', () => {
     window.history.pushState({}, '', `${window.location.origin}/registro`);
+    /* ----- Dispara manualmente el evento popstate para actualizar la ruta ----- */
+    window.dispatchEvent(new PopStateEvent('popstate'));
+    window.location.reload();
+  });
+
+  btnIniciarSesion.addEventListener('click', () => {
+    window.history.pushState({}, '', `${window.location.origin}/feed`);
     /* ----- Dispara manualmente el evento popstate para actualizar la ruta ----- */
     window.dispatchEvent(new PopStateEvent('popstate'));
     window.location.reload();
