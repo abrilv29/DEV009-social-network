2c8020c7acac950f8f8416f6140fc8753e061f76
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-08-02 16:47:26 -0500

    Se agregan cambios de  estilos en el feed

--- src/controller/feedController.js
+++ src/controller/feedController.js
@@ -1,27 +1,31 @@
-import { app } from "../lib/config-firebase";
-import { getFirestore, collection, addDoc, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { app } from '../lib/config-firebase';
 
-const db = getFirestore(app)
-
-
-
+const db = getFirestore(app);
 
 export async function guardarPost(datos) {
-    try {
-        const documento = await addDoc(collection(db, "posts"), datos);
+  try {
+    const documento = await addDoc(collection(db, 'posts'), datos);
 
-        console.log(datos);
-        console.log("Document written with ID: ", documento.id);
-        return documento
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-
+    console.log(datos);
+    console.log('Document written with ID: ', documento.id);
+    return documento;
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+  return null;
 }
 
 export async function traerpost() {
-    console.log('Funcion traer');
-    const todosLosPosts =  query(collection(db, "posts"), orderBy("created_date", "desc"));
-    const documentos = await getDocs(todosLosPosts);
-    return documentos
+  console.log('Funcion traer');
+  const todosLosPosts = query(collection(db, 'posts'), orderBy('created_date', 'desc'));
+  const documentos = await getDocs(todosLosPosts);
+  return documentos;
 }
