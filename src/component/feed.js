cc2ce83f5a26edde09a7b21600c75a1f6dd51937
Abril <abrilv29@gmail.com>
2023-08-03 01:26:14 -0600

    Cambios en login y feed parte3

--- src/component/feed.js
+++ src/component/feed.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { createElement } from '../utils/utils';
 import { guardarPost, traerpost } from '../controller/feedController';
 
@@ -33,11 +34,9 @@ export function feedView() {
   buttonPublicar.setAttribute('type', 'button');
   // buttonPublicar.setAttribute('id', 'boton_publicar');
 
-  createElement('section', 'section_publicaciones', sectionFeed);
-  console.log(dibujarPosts());
-  dibujarPosts();
+  const sectionPublic = createElement('section', 'section_publicaciones', sectionFeed);
+  sectionPublic.setAttribute('id', 'section_publicaciones');
 
-  // Publicar post
   buttonPublicar.addEventListener('click', async () => {
     const valuePublicacion = inputPublicacion.value;
 
@@ -45,61 +44,69 @@ export function feedView() {
       alert('Los campos no pueden estar vacios');
     } else {
       const datos = {
-        // user: "Pepito",
-        // last: "Perez",
         created_date: new Date(),
-        // edited_date: "",
         post: valuePublicacion,
-        // likes: "",
       };
       inputPublicacion.value = '';
       await guardarPost(datos);
 
+      // Limpia el contenido de las publicaciones existentes
       const sectionPublicaciones = document.querySelector('.section_publicaciones');
       sectionPublicaciones.innerHTML = '';
-      // createPost(datos);
-      // traerpost();
-      dibujarPosts();
+
+    // Luego obtenemos los nuevos posts de todos los usuarios
+    const nuevosPosts = await traerpost();
+
+    // Y finalmente dibujamos todos los posts actualizados
+    dibujarPosts(nuevosPosts);
     }
-  });
+  }); // btnPublicar
 
   return sectionFeed;
 }
 
-// Funcion dibujar posts
-async function dibujarPosts() {
-  const documentos = await traerpost();
+// ...
 
-  documentos.forEach((doc) => {
-    console.log(doc.data());
-    createPost(doc.data());
+// Funcion dibujar posts
+function dibujarPosts(posts) {
+  const sectionPublicaciones = document.querySelector('.section_publicaciones');
+  if (!sectionPublicaciones) {
+    // Si el elemento con la clase 'section_publicaciones' no existe, salimos de la función.
+    return;
+  }
+
+  sectionPublicaciones.innerHTML = ''; // Limpiamos el contenido actual
+
+  posts.forEach((datos) => {
+    const divPublicacion = createElement('div', 'container_publicacion', sectionPublicaciones);
+    const name = createElement('p', 'name_user', divPublicacion);
+    // Obtener el nombre de usuario del localStorage
+    // const userName = localStorage.getItem('userName');
+    // Mostrar el nombre de usuario en el elemento 'name'
+    name.textContent = datos.author ? `Publicado por: ${datos.author}` : 'Autor Desconocido';
+    const textarea = createElement('textarea', 'textarea_publicacion', divPublicacion);
+    textarea.setAttribute('disabled', '');
+    // Verificar si el campo 'post' existe en la propiedad 'datos'
+    if (datos.datos && datos.datos.datos) {
+      textarea.value = datos.datos.datos; // Usar 'value' en lugar de 'innerHTML'
+    } else if (datos.datos && datos.datos.post) {
+      textarea.value = datos.datos.post; // Usar 'value' en lugar de 'innerHTML'
+    } else {
+      textarea.value = 'Contenido no encontrado'; // En caso de que no se encuentre el campo 'post'
+    }
+    const sectionInteracion = createElement('section', 'like_edit_delete', divPublicacion);
+    const btnedit = createElement('button', 'icono_edit', sectionInteracion);
+    btnedit.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+    const iconoDelete = createElement('button', 'icono_delete', sectionInteracion);
+    iconoDelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    const like = createElement('button', 'icono_like', sectionInteracion);
+    like.innerHTML = '<i class="fa-regular fa-heart"></i>';
+    const contador = createElement('label', 'contador_like', sectionInteracion);
+    contador.innerHTML = '0';
   });
 }
 
-// Funcion crear publicaciones
-function createPost(datos) {
-  // Seccion publicaciones
-  // const sectionPublicaciones = createElement('section', 'section_publicaciones', sectionFeed);
-  const sectionPublicaciones = document.querySelector('.section_publicaciones');
-  const divPublicacion = createElement('div', 'container_publicacion', sectionPublicaciones);
-  const name = createElement('p', 'name_user', divPublicacion);
-  // Obtener el nombre de usuario del localStorage
-  const userName = localStorage.getItem('userName');
-  // Mostrar el nombre de usuario en el elemento 'name'
-  name.textContent = userName ? `Publicado por: ${userName}` : 'Usuario Anónimo';
-  const textarea = createElement('textarea', 'textarea_publicacion', divPublicacion);
-  textarea.setAttribute('disabled', '');
-  textarea.innerHTML = datos.post;
-  const sectionInteracion = createElement('section', 'like_edit_delete', divPublicacion);
-  const btnedit = createElement('button', 'icono_edit', sectionInteracion);
-  btnedit.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
-  const iconoDelete = createElement('button', 'icono_delete', sectionInteracion);
-  iconoDelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  const like = createElement('button', 'icono_like', sectionInteracion);
-  like.innerHTML = '<i class="fa-regular fa-heart"></i>';
-  const contador = createElement('label', 'contador_like', sectionInteracion);
-  contador.innerHTML = '0';
-}
+// ...
 
 window.addEventListener('DOMContentLoaded', () => {
   const mensajeBienvenida = document.getElementById('mensaje_bienvenida');
@@ -107,4 +114,5 @@ window.addEventListener('DOMContentLoaded', () => {
   if (mensajeBienvenida) {
     mensajeBienvenida.textContent = `¡Bienvenido, ${userName}!`;
   }
+  traerpost(dibujarPosts);
 });
--- src/controller/feedController.js
+++ src/controller/feedController.js
@@ -1,14 +1,19 @@
 import {
-  getFirestore, collection, addDoc, getDocs, orderBy, query,
+  getFirestore, collection, addDoc, onSnapshot, orderBy, query,
 } from 'firebase/firestore';
 import { app } from '../lib/config-firebase';
 
+// const auth = getAuth();
 const db = getFirestore(app);
 
 export async function guardarPost(datos) {
   try {
-    const documento = await addDoc(collection(db, 'posts'), datos);
-
+    const documento = await addDoc(collection(db, 'posts'), {
+      datos,
+      created_date: Date.now(),
+      author: localStorage.getItem('userName'),
+      likes: 0,
+    });
     console.log(datos);
     console.log('Document written with ID: ', documento.id);
     return documento;
@@ -18,9 +23,58 @@ export async function guardarPost(datos) {
   return null;
 }
 
-export async function traerpost() {
-  console.log('Funcion traer');
+/*export async function traerpost(callback) {
+  const userName = localStorage.getItem('userName');
+
+  if (!userName) {
+    // Si no se encuentra el nombre de usuario en el Local Storage, no hacemos nada.
+    return;
+  }
+  console.log('Funcion traer post en tiempo real');
   const todosLosPosts = query(collection(db, 'posts'), orderBy('created_date', 'desc'));
-  const documentos = await getDocs(todosLosPosts);
-  return documentos;
+  const newPost = onSnapshot(todosLosPosts, (querySnapshot) => {
+    const postPorUsers = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.data().author === userName) {
+        postPorUsers.push(doc.data());
+      }// cierra el if
+    });// cierra el forEach
+    callback(postPorUsers); //
+  });
+  // eslint-disable-next-line consistent-return
+  return newPost;
+}*/
+/*export async function traerpost(callback) {
+  const userName = localStorage.getItem('userName');
+
+  if (!userName) {
+    // Si no se encuentra el nombre de usuario en el Local Storage, no hacemos nada.
+    return;
+  }
+  console.log('Funcion traer post en tiempo real');
+  const todosLosPosts = query(collection(db, 'posts'), orderBy('created_date', 'desc'));
+  const newPost = onSnapshot(todosLosPosts, (querySnapshot) => {
+    const postPorUsers = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.data().author === userName) {
+        postPorUsers.push(doc.data());
+      }// cierra el if
+    });// cierra el forEach
+    console.log('Datos recibidos:', postPorUsers); // Agrega este log para verificar los datos recibidos
+    callback(postPorUsers); //
+  });
+  // eslint-disable-next-line consistent-return
+  return newPost;
+}*/
+export async function traerpost(callback) {
+  const todosLosPosts = query(collection(db, 'posts'), orderBy('created_date', 'desc'));
+  const newPost = onSnapshot(todosLosPosts, (querySnapshot) => {
+    const postPorUsers = [];
+    querySnapshot.forEach((doc) => {
+      postPorUsers.push(doc.data());
+    });
+    console.log('Datos recibidos:', postPorUsers);
+    callback(postPorUsers);
+  });
+  return newPost;
 }
