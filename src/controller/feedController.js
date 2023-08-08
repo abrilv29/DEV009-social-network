67ac4a4be6a715d79d524c08c9d5e9271a077841
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-08-08 17:54:53 -0500

    Se agrega funcion para agregar, remover y mostrar la cantidad de los likes

--- src/controller/feedController.js
+++ src/controller/feedController.js
@@ -5,6 +5,10 @@ import {
   getDocs,
   orderBy,
   query,
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
 } from 'firebase/firestore';
 import { app } from '../lib/config-firebase';
 
@@ -13,19 +17,34 @@ const db = getFirestore(app);
 export async function guardarPost(datos) {
   try {
     const documento = await addDoc(collection(db, 'posts'), datos);
-
-    console.log(datos);
-    console.log('Document written with ID: ', documento.id);
+    // console.log('Document written with ID: ', documento.id);
     return documento;
   } catch (e) {
-    console.error('Error adding document: ', e);
+    // console.error('Error adding document: ', e);
   }
   return null;
 }
 
 export async function traerpost() {
-  console.log('Funcion traer');
   const todosLosPosts = query(collection(db, 'posts'), orderBy('created_date', 'desc'));
   const documentos = await getDocs(todosLosPosts);
   return documentos;
+}
+
+// Likes
+export async function addLiked(userId, idPost) {
+  const documentoPosts = doc(db, 'posts', idPost);
+
+  // Atomically add a new usuario to the "likes" array field.
+  await updateDoc(documentoPosts, {
+    likes: arrayUnion(userId),
+  });
+}
+
+export async function removeLiked(userId, idPost) {
+  const documentoPosts = doc(db, 'posts', idPost);
+  // Atomically remove a usuario from the "likes" array field.
+  await updateDoc(documentoPosts, {
+    likes: arrayRemove(userId),
+  });
 }
